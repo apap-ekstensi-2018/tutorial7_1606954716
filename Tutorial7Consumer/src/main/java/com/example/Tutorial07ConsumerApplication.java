0cba3192b3052e52b563f8896c44495f6d88ce5a
= <akhdakhz@gmail.com>
2018-04-10 20:54:38 +0700

    first commit

---
+++ Tutorial7Consumer/src/main/java/com/example/Tutorial07ConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial07ConsumerApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (Tutorial07ConsumerApplication.class, args);
+    }
+}
---
+++ Tutorial7Producer/src/main/java/com/example/Tutorial07Producer.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial07Producer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial07Producer.class, args);
+    }
+}
